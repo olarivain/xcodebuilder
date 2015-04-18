7785df91903141959f20e874d5466063d8940ef4
Kra Larivain <olarivain@gmail.com>
2015-04-18 02:42:10 -0700

    watch app support

--- lib/xcode_builder/configuration.rb
+++ lib/xcode_builder/configuration.rb
@@ -22,6 +22,8 @@ module XcodeBuilder
       end
 
       args << "-sdk iphoneos"
+
+      args << "-archivePath #{File.expand_path xcarchive_path}"
       
       args << "-configuration '#{configuration}'"
       args << "BUILD_DIR=\"#{File.expand_path build_dir}\""
@@ -133,6 +135,10 @@ module XcodeBuilder
     
     def built_app_path
       File.join("#{build_dir}", "#{configuration}-iphoneos", "#{app_file_name}")
+    end
+
+    def xcarchive_path
+      File.join("#{build_dir}", "#{configuration}.xcarchive")
     end
 
     def ipa_path
